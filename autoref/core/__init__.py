66f305c8997099a1b8e47c8d2b4d22d1f2e259c7
JuanM4T <plmauna@gmail.com>
2026-04-29 22:53:36 +0200

    refactor: emptied out __init__.py from core/

--- autoref/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .enums import WinCondition, MapState, Step, RefMode
-from .models import PlayableMap, Pool, ModdedPool, Team, Ruleset, Match, Timers, NO_MODS, OrderScheme
-from .storage import MatchDatabase
-from .lobby import Lobby, MatchResult, PlayerResult, SlotInfo
-from .output import OutputSink
-from .base import AutoRef
