b0dff5dbcbf42ccb44a50608af35397f6e555798
JuanM4T <plmauna@gmail.com>
2026-04-29 22:41:48 +0200

    refactor: rm hard dep from client.py

--- autoref/client.py
+++ autoref/client.py
@@ -1,11 +1,24 @@
-from dotenv import load_dotenv
 from os import getenv
 import aiosu
 
-load_dotenv()
+_DOTENV_LOADED = False
+
+
+def _ensure_dotenv() -> None:
+    """Load .env once on first client construction. Idempotent and side-effect-free at import time."""
+    global _DOTENV_LOADED
+    if _DOTENV_LOADED:
+        return
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+    except ImportError:
+        pass
+    _DOTENV_LOADED = True
 
 
 def make_client() -> aiosu.v2.Client:
+    _ensure_dotenv()
     return aiosu.v2.Client(
         client_id=getenv("CLIENT_ID"),
         client_secret=getenv("CLIENT_SECRET"),
